00728a125fd5412b8b21a622fea4972e5d553a36
dizzi <gusrl4025@gmail.com>
2021-05-17 20:00:47 +0900

    upload file to s3 using boto3 successfully

--- driver/s3_bucket.py
+++ driver/s3_bucket.py
@@ -1,34 +1,36 @@
 import boto3
 import logging
 import json
+import os
+from django.conf import settings 
 from botocore.exceptions import ClientError
 
+# BASE_DIR = settings.BASE_DIR
+# MEDIA_DIR = settings.MEDIA_ROOT
 s3 = boto3.client(
     's3',
-    aws_access_key_id='AWS_ACCESS_KEY',
-    aws_secret_access_key='AWS_SECRET_ACCESS_KEY'   
+    aws_access_key_id='ASIAVJ7ZLDLC5QVI57WO',
+    aws_secret_access_key='lYR7Ute0YeGAKj2aWFVp0mBUZ7dXhQmwHiE+HN0C',
+    aws_session_token='IQoJb3JpZ2luX2VjEJL//////////wEaCXVzLXdlc3QtMiJGMEQCIGSvj9XwfVYiEWCwVsbfC+5RNGFdvsGUAXAP7ICcSNAOAiA8oB4w6q1AGJfeAVc/2+/xtRFVIAYjRDjyopY4aP6UJiqrAggrEAAaDDM2NTA1ODI2Nzg0NSIMhHQ4xuNl8X8nswBIKogCJ7Ttx0QRiG1XpahSHq7N5IUn6LhPyR1EC+BYKSo34mjpqY4MFG7VDzB9PbO0X3Ef8JdWXWHjVaEmHdgqbHgrHuAhC7cO5YPiZOHD6qT9uyF95UcfejvnQPD/CT6vsqBAF6WCDxyXc7U8hKgk/T2Fbt5s+iZU5AZ2vL1GTBv/hGZ5XI4aTIAaM1YTJzwY+gA/apno/F9OREFpKr0J5Tc3SG00g2lk6Gb4/VfekCAG/sGZ4LxYM7088YsbEnBFT091DRit1zUqVb1cePX2JVI7CJwWifpm7SCGQCkw6RYBfK1Yz/Vh3JycC+YaRTFYh+IFnqJtOaSr5VWX6qu3I1UWBodRG0RppRpaMMDTg4UGOp4BQZRUdT8w45Fa2lsghMW98Abr0EbNSxav9EKHFt+N/MkwjgbgHwhEAnm4svcXOePlheoRBCDK3ycPqmgLnomcZ8EwW88wPJ/ZfXchjMzSEqC8goAQgQl2Eo/QWtX3lcUPEADTEwVJygBHc4mp/GvMZ83X18leA8JLy6yLu6Vc3qUHn76MAF1wYnlWyRBzA4RXZhCw0nFe67iQ0WaqI/0='
 )
 
-BUCKET = 'khugle-drive-test2'
- 
+BUCKET = 'khugle-drive-test'
+
 def upload_file(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
+    
     # If S3 object_name was not specified, use file_name
     if object_name is None:
         object_name = file_name
+    # Upload the fil
+    response = s3.upload_file(file_name, bucket, object_name)
 
-    # Upload the file
-    try:
-        response = s3.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
+def create_bucket(bucket_name, region=None):
+    s3.create_bucket(Bucket=bucket_name)
 
+def list_bucket():
+    res = s3.list_buckets()
+    return [bucket['Name'] for bucket in res['Buckets']]
+
+res = upload_file('C:\\Users\\dizzi\\Desktop\\KHUgle_drive\\static\\driver\\file\\text.txt', 'khugle-drive-test', 'image/test4.txt')
+
+#res = upload_file(os.path.join(MEDIA_DIR, 'text.txt'), 'khugle-drive-test', 'image/test.txt')
--- driver/views.py
+++ driver/views.py
@@ -4,6 +4,8 @@ from rest_framework import permissions
 from .models import Article
 from .serializers import ArticleSerializer
 import driver.s3_bucket as s3
+import os
+from django.conf import settings
 # Create your views here.
 
 class ArticleView(viewsets.ModelViewSet):
@@ -11,7 +13,8 @@ class ArticleView(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
-
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-        s3.upload_file('static/driver/file/text.txt', 'khugle-drive-test2', 'image/test.txt')
+        file_path = serializer.data.get('file')
+        file_name = file_path.split('/')[-1]
+        s3.upload_file(os.path.join(settings.MEDIA_ROOT, file_name), 'khugle-drive-test', 'image/test2.txt')
